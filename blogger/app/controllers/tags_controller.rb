734524951b7ef61c5f41773f846dd9a8f2195270
voidn0ise <ksixevolutionx@hotmail.com>
2017-08-01 17:18:05 -0400

    Added comments, users, tags, and authentication via sorcery. Project complete.

---
+++ blogger/app/controllers/tags_controller.rb
@@ -0,0 +1,21 @@
+class TagsController < ApplicationController
+  before_action :require_login, only [:destroy]
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.destroy(params[:id])
+    @tag.save
+
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+    redirect_to tags_path
+  end
+
+end
