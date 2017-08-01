45f5e3ee6ec3d14511ac972ee3140493fdafe742
voidn0ise <ksixevolutionx@hotmail.com>
2017-08-01 17:29:39 -0400

    Updates for Heroku Deployment

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -1,5 +1,5 @@
 class TagsController < ApplicationController
-  before_action :require_login, only [:destroy]
+  before_action :require_login, only: [:destroy]
 
   def show
     @tag = Tag.find(params[:id])
